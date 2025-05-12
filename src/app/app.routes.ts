266c77fad42c865ba7c63e477bca7854b2cde8fa
Denis <denisixcajoc2001@gmail.com>
2025-05-11 20:16:08 -0600

    add: ✨Configuracion de rutas, estilos boostrap y Angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import HomeComponent from './componentes/home/home.component';
+import PunoComponent from './componentes/puno/puno.component';
+import { PdosComponent } from './componentes/pdos/pdos.component';
+import PdragComponent from './componentes/pdrag/pdrag.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home Page',
+    },
+    {
+        path: "puno",
+        component: PunoComponent,
+        title: 'puno',
+    },
+    {
+        path: "pdos",
+        component: PdosComponent,
+        title: 'Pdos',
+    },
+    {
+        path: "pdrag",
+        component: PdragComponent,
+        title: 'pgrag',
+    },
+    // {
+    //     path: "details/:id",
+    //     component: DetailsComponent,
+    //     title: 'Home Datails',
+    // },
+];
